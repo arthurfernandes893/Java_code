2d33e14c15cb42d85669495a7168b8194bf4049f
Arthur <arthurfernandes893@gmail.com>
2023-05-02 14:57:39 -0300

    ciracao de primeiras classes

---
+++ programa1/P1nX.java
@@ -0,0 +1,27 @@
+import Pessoa.java; // assim ou usa o comando package???//
+
+public class P1nX{
+    public class Homem extends Pessoa{
+        public String genero;
+        public int idade;
+
+        public Homem(String nomelido,String sobrenomelido,GregorianCalendar datalida,int numcpflido, float pesolido, float alturalida, String genero, int idade){
+            super(nomelido, sobrenomelido, datalida, numcpflido, pesolido, alturalida)
+            this.genero = genero;
+            this.idade = idade;
+        }
+    }
+    public class Mulher extends Pessoa{
+        public String genero;
+        public int idade;
+
+        public Homem(String nomelido,String sobrenomelido,GregorianCalendar datalida,int numcpflido, float pesolido, float alturalida, String genero, int idade){
+            super(nomelido, sobrenomelido, datalida, numcpflido, pesolido, alturalida)
+            this.genero = genero;
+            this.idade = idade;
+        }
+    }
+    public static void main(String[] args){
+        
+    }
+}
---
+++ programa1/Pessoa.java
@@ -0,0 +1,41 @@
+public class Pessoa{
+//declaracao de campos
+   private String nome;
+   private String Sobrenome;
+   private GregorianCalendar dataNasc; //importar//
+   private int numCPF;
+   
+   public float peso;
+   public float altura;
+
+//construtor para caso ideal com todas as infos passadas
+   Public Pessoa(String nomelido,String sobrenomelido,GregorianCalendar datalida,int numcpflido, float pesolido, float alturalida){
+    nome = nomelido;
+    sobrenome = sobrenomelido;
+    data = datalida;
+    /* ou
+    data.year = datalida.year;
+    data.month = datalida.month;
+    data.dayOfMonth = datalida.dayOfMonth
+    */
+    numCPF = numcpflido;
+    peso = pesolido;
+    altura = alturalida;
+}
+
+//metodos getter para informacoes sensiveis
+public String getnome(){
+    return nome;
+}
+public String getsobrenome(){
+    return sobrenome;
+}
+public GregorianCalendar getdataNasc(){
+    return dataNasc;
+}
+public int getnumCPF(){
+    return numCPF;
+}
+
+
+}
